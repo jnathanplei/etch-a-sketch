7d7f48ba28f67a4aa4db46e46a0ff871cdbd1bf3
Nathan Lei <jnathanplei@gmail.com>
2022-07-24 03:32:25 -0700

    add a button that clears existing grid and make a new grid with user specified size

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const container = document.getElementById("container");
-
 function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
+    const container = document.getElementById("container");
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
     //cell.innerText = (c+1);
-    container.appendChild(cell).className = "grid-item";
-  };
+        container.appendChild(cell).className = "grid-item";
+    };
 };
 
 makeRows(16, 16);
@@ -20,3 +19,27 @@ function blackColor() {
 }
 
 blackColor()
+
+//clear and make new grid
+
+function clearRows() {
+    document.getElementById('container').textContent = '';
+}
+
+var button = document.createElement("button");
+button.innerHTML = "Change Grid Size";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+    clearRows();
+    let size = "";
+    while (size>100 || size<2) {
+        size = prompt("Grid Size? 2 to 100", 0);
+    }
+    rows = parseInt(size);
+    cols = parseInt(size);
+    makeRows(rows, cols);
+    blackColor()
+});
